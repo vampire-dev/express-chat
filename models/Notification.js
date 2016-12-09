a887d108a7fdb001686f55a6b57eb50db0992dcd
Vai Wilmintje <Vai Wilmintje>
2016-12-09 10:45:40 +0700

    add notification model

--- models/Notification.js
+++ models/Notification.js
@@ -1,5 +1,10 @@
 "use strict";
-exports.attributes = {};
+const sequelize = require('sequelize');
+exports.attributes = {
+    id: { type: sequelize.BIGINT, allowNull: false, primaryKey: true, autoIncrement: true },
+    profileId: { type: sequelize.BIGINT, allowNull: false, field: 'profile_id' },
+    messageId: { type: sequelize.BIGINT, allowNull: false, field: 'message_id' }
+};
 exports.options = {
     freezeTableName: true,
     tableName: 'notifications',
--- models/Notification.ts
+++ models/Notification.ts
@@ -2,11 +2,15 @@
 import {Instance} from './BaseModel';
 
 export interface INotification {
-   
+    id: number;
+    profileId: number;
+    messageId: number;
 }
 
 export var attributes: sequelize.DefineAttributes = {
-   
+    id: { type: sequelize.BIGINT, allowNull: false, primaryKey: true, autoIncrement: true },
+    profileId: { type: sequelize.BIGINT, allowNull: false, field: 'profile_id' },
+    messageId: { type: sequelize.BIGINT, allowNull: false, field: 'message_id' }
 }
 
 export var options: sequelize.DefineOptions<Instance<INotification>> = {
